00315609d98c41710b7b48f1badad6ce39eb1b61
Daniel Prastha <danielprastha4@gmail.com>
2024-01-22 08:51:28 +0700

    set user id to nullable

---
+++ Migrations/20240122014759_SetUserIdToNullableInTodoItemTable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_TodoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetUserIdToNullableInTodoItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "TodoItems",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "TodoItems",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
